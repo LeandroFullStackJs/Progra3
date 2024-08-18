41e51291c1cc0aec5c694ba298469ba4add92742
Lean <leandroEzequiel2014@gmail.com>
2024-08-18 16:16:33 -0300

    Update SumaDeEnterosRecursivo.java

--- src/actividad1/act1_3/SumaDeEnterosRecursivo.java
+++ src/actividad1/act1_3/SumaDeEnterosRecursivo.java
@@ -8,3 +8,30 @@ public final class SumaDeEnterosRecursivo {
         return sumarEnterosRecursivamente(nro-1) + nro;
     }
 }
+
+// OTRA FORMA DE HACERLO : 
+    // Método para realizar la búsqueda binaria
+   /*  public static int busquedaBinaria(int[] arreglo, int objetivo) {
+        int izquierda = 0;
+        int derecha = arreglo.length - 1;
+
+        while (izquierda <= derecha) {
+            int medio = izquierda + (derecha - izquierda) / 2;
+
+            
+            if (arreglo[medio] == objetivo) {
+                return medio; // Retorna el índice del objetivo
+            }
+            if (arreglo[medio] < objetivo) {
+                izquierda = medio + 1;
+            } 
+            
+            else {
+                derecha = medio - 1;
+            }
+        }
+
+        
+        return -1;
+    }
+        */
