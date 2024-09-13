4ef9c6a3eb5ba74077669c9a6584feae7c2d082a
EnzoMarco01 <emarco@uade.edu.ar>
2024-09-12 21:58:43 -0300

    clase 3 act 1

--- src/clase4.java
+++ src/clase4.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class CambioMonedas {
+class CambioMonedas {
 
     public static boolean puedeDarCambioExacto(int[] monedas, int monto) {
         // Ordenar las monedas en orden descendente
